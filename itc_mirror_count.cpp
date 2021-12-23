cbe57ccaae941f2b7a505627e38eb6c64094f77b
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-12-23 12:10:22 +0300

    Update itc_mirror_count.cpp

--- itc_mirror_count.cpp
+++ itc_mirror_count.cpp
@@ -4,10 +4,11 @@ int itc_mirror_count(long long a){
     int b = 0;
     if (a > 0){
         for (long long i = 1; i <= a; i++){
-        if (itc_mirror_num(i))
-            b++;}}
+            if (itc_mirror_num(i))
+                b++;}}
     else{
         for (long long i = a ; i <= 1; i++){
         if (itc_mirror_num(i)){
             b++;
-    }}}
+    }}
+    return b;}
